852bfe4ba54ed8a12d32e6995725a5440707c0a3
Pascal <pascal.ganaye@itron.com>
2020-10-03 12:54:16 +0200

    add chai because it integrates well with mocha-sidebar

--- test/test1.ts
+++ test/test1.ts
@@ -1,10 +1,11 @@
+import { assert, expect } from "chai"
 import { Dog } from "../src/Dog";
 import { Cat } from "../src/Cat";
-var assert = require('assert');
 
 describe('test1', function() {
   it('The dog should bark', function() {
     let dog = new Dog();
     assert.equal("woof", dog.sound());
+    expect(1).to.equal(1)
   });
 });
